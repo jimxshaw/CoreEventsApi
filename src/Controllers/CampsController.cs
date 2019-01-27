89c85312b8c1b875d5d779d66a6c8ebff1d1dc2d
Jim Shaw <jimxshaw@gmail.com>
2019-01-27 15:18:23 -0600

    Add sample Get method in CampsController.

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodeCamp.Controllers
 {
-    public class CampsController
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
     {
-        
+        public object Get() 
+        {
+            return new { Moniker = "TX2020", Name = "Texas Code Camp 2020" };
+        }
     }
 }
