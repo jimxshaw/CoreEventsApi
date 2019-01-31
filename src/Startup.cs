402c5611684da4d2e93da601a99a1e1baebec7ae
Jim Shaw <jimxshaw@gmail.com>
2019-01-31 11:03:34 -0600

    Register automapper within Startup class.

--- src/Startup.cs
+++ src/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -34,6 +35,8 @@ namespace CoreCodeCamp
                     .BuildServiceProvider();
 
             services.AddScoped<ICampRepository, CampRepository>();
+
+            services.AddAutoMapper();            
 
             services.AddMvc()
                     .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
