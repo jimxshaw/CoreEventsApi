b8e8b26ef1fb03847940628537c747324c4ab1ce
Jim Shaw <jimxshaw@gmail.com>
2019-02-05 10:27:33 -0600

    Update mapping of Camp to CampModel in CampProfile.cs.

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -10,8 +10,15 @@ namespace CoreCodeCamp.Data
     public class CampProfile : Profile
     {
         public CampProfile()
-        {
-            this.CreateMap<Camp, CampModel>();
+        {            
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(cm => cm.Venue, o => o.MapFrom(c => c.Location.VenueName))
+                .ForMember(cm => cm.Address1, o => o.MapFrom(c => c.Location.Address1))
+                .ForMember(cm => cm.Address2, o => o.MapFrom(c => c.Location.Address2))
+                .ForMember(cm => cm.Address3, o => o.MapFrom(c => c.Location.Address3))
+                .ForMember(cm => cm.CityTown, o => o.MapFrom(c => c.Location.CityTown))
+                .ForMember(cm => cm.StateProvince, o => o.MapFrom(c => c.Location.StateProvince))
+                .ForMember(cm => cm.PostalCode, o => o.MapFrom(c => c.Location.PostalCode));
         }
     }
 }
